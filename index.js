eea4e09e6579fa188e5d4e377729827db07dab37
Uilque Messias <uqmessias@gmail.com>
2024-07-03 01:49:34 -0300

    refactor: move code to src folder

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import {AppRegistry, Platform} from 'react-native';
+
+import App from './src/App';
+import app from './app.json';
+
+const {name: appName} = app;
 
 AppRegistry.registerComponent(appName, () => App);
