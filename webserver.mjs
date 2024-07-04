2af1f0aedc7142520ad5984fb3d75e8260f066e6
Uilque Messias <uqmessias@gmail.com>
2024-07-04 03:22:39 -0300

    refactor: rename the index.html script based on the timestamp

--- webserver.mjs
+++ webserver.mjs
@@ -1,26 +1,46 @@
-import {build} from 'esbuild';
-import devServer from 'esbuild-dev-server';
+import esbuild from 'esbuild';
+// import devServer from 'esbuild-dev-server';
 import open from 'open';
+import fs from 'fs';
+import dayjs from 'dayjs';
 
-const buildPromise = build({
-  entryPoints: ['index.js'],
-  bundle: true,
-  outfile: './dist/js/bundle.web.js',
-  alias: {
-    'react-native': 'react-native-web',
+const version = dayjs().valueOf();
+const servedir = './dist';
+
+const templateCache = {
+  name: 'templateCache',
+  setup(build) {
+    build.onEnd(() => {
+      const index = fs.readFileSync('./web/template.html', {encoding: 'utf-8'});
+      const newIndex = index.replaceAll('%%VERSION%%', version);
+
+      if (!fs.existsSync(servedir)) {
+        fs.mkdirSync(servedir);
+      }
+
+      fs.writeFileSync(`${servedir}/index.html`, newIndex);
+    });
   },
-  loader: {
-    '.js': 'tsx',
-  },
-});
+};
 
-const port = '8080';
+(async function () {
+  const cxt = await esbuild.context({
+    entryPoints: ['index.js'],
+    bundle: true,
+    outfile: `${servedir}/js/bundle.web.${version}.js`,
+    alias: {
+      'react-native': 'react-native-web',
+    },
+    loader: {
+      '.js': 'tsx',
+    },
+    plugins: [templateCache],
+  });
 
-devServer.start(buildPromise, {
-  index: 'web/index.html',
-  port,
-  watchDir: 'src',
-  staticDir: 'dist',
-});
+  // const port = '8080';
+  let {port} = await cxt.serve({servedir}).catch(async () => {
+    await cxt.dispose();
+  });
 
-open(`http://localhost:${port}`);
+  open(`http://localhost:${port}`);
+})();
